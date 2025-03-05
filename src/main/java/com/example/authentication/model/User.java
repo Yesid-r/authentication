f38888e27cf97de06702fd39ac3ffef1f2e69d7e
DAIRO YESSID RINCON CARREÑO <dairo.rincon@uptc.edu.co>
2025-03-05 09:15:59 -0500

    configuracion inical del modelo y configuracion de docker  para base de datos postgresql

---
+++ src/main/java/com/example/authentication/model/User.java
@@ -0,0 +1,10 @@
+package com.example.authentication.model;
+
+public class User {
+
+    private Long id;
+    private String email;
+    private  String password;
+    private Boolean isVerified;
+    private Role role;
+}
---
+++ src/main/java/com/example/authentication/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.authentication.repository;
+
+import com.example.authentication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
